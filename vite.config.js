30680b8be6ab0cf7aab19b195b4694af7ef036ed
Aya Sareej <ayakillua2@gmail.com>
2024-10-28 22:43:22 +0200

    just update the photos path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/landscaping-company/",
+  base: command === "build" ? "/landscaping-company/" : "/",
   plugins: [react()],
   build: {
     rollupOptions: {
